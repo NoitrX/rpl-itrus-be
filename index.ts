644a458d30b2d86cd91910868232a0bd6a6daced
Naufhal <naufhalsimdigxrpl2@gmail.com>
2024-07-02 17:55:58 +0700

    Dummy Data

--- index.ts
+++ index.ts
@@ -6,19 +6,30 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 async function main() {
-  const hashPw = await bcrypt.hash("password", 10);
-  const user = await prisma.user.create({
+  // const hashPw = await bcrypt.hash("password", 10);
+  // const user = await prisma.user.create({
+  //   data: {
+  //     username: "admin",
+  //     nim: "2307411013",
+  //     email: "admin@gmail.com",
+  //     password: hashPw,
+  //     level: "ADMIN",
+  //     status: "active",
+  //   },
+  // });
+  // console.log(user);
+  // console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
+  const post = await prisma.interaction_Like.create({
     data: {
-      username: "admin",
-      nim: "2307411013",
-      email: "admin@gmail.com",
-      password: hashPw,
-      level: "ADMIN",
-      status: "active",
+      liked: true,
+      postId: 2,
+      user_id: 1,
+      createdAt: new Date(),
     },
   });
-  console.log(user);
-  console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
+  console.log(post);
 }
 
 main()
